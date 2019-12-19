96624ed80752bda76029437246d32936e2f64adc
Confidence Oguka <ogukaconfidence@gmail.com>
2019-12-19 12:22:49 +0100

    drop it here

---
+++ index.php
@@ -0,0 +1,15 @@
+<div class="col-md-5 bg-success">
+                        <figure id="logo-holder" >
+                            <img class="img-fluid logo" src="images/rose1.png">
+                            <label class="logo_title">THE ROSE TONY FOUNDATION</label>
+                        </figure>
+                    </div>
+                    <div class="col-md-6 bg-info">
+                        <nav class="">
+                            <a href="">Home</a>
+                            <a href="">About Us</a>
+                            <a href="">Gallery</a>
+                            <a href="">Programs</a>
+                            <a href=""></a>
+                        </nav>
+                    </div>
